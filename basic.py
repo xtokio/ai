bac74a94cd440aa3626f820f47d0f272b0fd0d5d
Luis Gomez <xtokio@gmail.com>
2023-12-28 15:17:25 -0500

    Added basic python script

--- basic.py
+++ basic.py
@@ -1,3 +1,4 @@
+# Basic example on how to call a local model using langchain and Ollama
 from langchain.llms import Ollama
 from langchain.callbacks.manager import CallbackManager
 from langchain.callbacks.streaming_stdout import StreamingStdOutCallbackHandler
